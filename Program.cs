07b6f78ab2ae3cdf0ed1559dd782b79d00fdd5c7
Kirill Delija <70683637+birdass@users.noreply.github.com>
2021-05-27 13:57:01 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,44 +1,86 @@
 ﻿using System;
+using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace zmeika
+namespace SnakeGame
 {
-    class Game {
-        static Walls walls; //объявили
-        static FoodFactory foodFactory;
-
-        static readonly int x = 80; // Задаем начальные точки
+    class Game
+    {
+        static readonly int x = 80; // х и у окна консоли
         static readonly int y = 26;
+        private static int PeriodTime = 200; // задержка передвижения
+        static Walls walls; // объявляем
+        static Snake snake;
+        static FoodFactory foodFactory;
+        static Timer time;
 
         static void Main()
         {
-            foodFactory = new FoodFactory(x, y, '@');
+            Console.SetWindowSize(x + 1, y + 1);
+            Console.SetBufferSize(x + 1, y + 1);
+            Console.CursorVisible = false;
+
+            walls = new Walls(x, y, '#'); // инициализируем
+            snake = new Snake(x / 2, y / 2, 3);
+
+            foodFactory = new FoodFactory(x, y, '¤');
             foodFactory.CreateFood();
-            walls = new Walls(x, y, '#'); // инициализировали
-            Console.SetWindowSize(x + 1, y + 1); // Размер окна
-            Console.SetBufferSize(x + 1, y + 1); // буфер окна консоли
-            Console.CursorVisible = false; // Скрыли курсор
+
+            time = new Timer(Loop, null, 0, PeriodTime);
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    snake.Rotation(key.Key);
+                }
+            }
+        }
+
+        static void Loop(object obj)
+        {
+            if (walls.IsHit(snake.GetHead()) || snake.IsHit(snake.GetHead()))
+            {
+                time.Change(0, Timeout.Infinite);
+            }
+            else if (snake.Eat(foodFactory.food))
+            {
+                foodFactory.CreateFood();
+                PeriodTime -= 5; // время на которое уменьшается время, следовательно увеличивается скорость, после еды 
+                PeriodTime = Math.Clamp(PeriodTime, 50, 100); // меньше 50 тайимер не опустится 
+                time.Change(0, PeriodTime);
+            }
+            else
+            {
+                snake.Move();
+            }
         }
     }
 
-    struct Point // создаем точку которая содержит координаиы и делаем методы для вывода на экран
+    struct Point 
     {
         public int x { get; set; }
         public int y { get; set; }
         public char ch { get; set; }
 
         public static implicit operator Point((int, int, char) value) =>
-            new Point { x = value.Item1, y = value.Item2, ch = value.Item3 };
+              new Point { x = value.Item1, y = value.Item2, ch = value.Item3 };
+
+        public static bool operator ==(Point a, Point b) =>
+                (a.x == b.x && a.y == b.y) ? true : false;
+        public static bool operator !=(Point a, Point b) =>
+                (a.x != b.x || a.y != b.y) ? true : false;
 
         public void Draw()
         {
             DrawPoint(ch);
         }
-        public void Clear() //стирание точки
+        public void Clear()
         {
             DrawPoint(' ');
         }
+
         private void DrawPoint(char _ch)
         {
             Console.SetCursorPosition(x, y);
@@ -46,22 +88,22 @@ namespace zmeika
         }
     }
 
-    class Walls //создаем класс стен
+    class Walls
     {
         private char ch;
         private List<Point> wall = new List<Point>();
 
-        public Walls(int x, int y, char ch)
+        public Walls(int x, int y, char ch) // отрисовка стен
         {
             this.ch = ch;
+
             DrawHorizontal(x, 0);
             DrawHorizontal(x, y);
             DrawVertical(0, y);
             DrawVertical(x, y);
-
         }
 
-        private void DrawHorizontal(int x, int y) // рисовка стен
+        private void DrawHorizontal(int x, int y) // создаем горизонтальную линию
         {
             for (int i = 0; i < x; i++)
             {
@@ -71,7 +113,7 @@ namespace zmeika
             }
         }
 
-        private void DrawVertical(int x, int y)
+        private void DrawVertical(int x, int y) // создаем вертикальную
         {
             for (int i = 0; i < y; i++)
             {
@@ -81,9 +123,142 @@ namespace zmeika
             }
         }
 
+        public bool IsHit(Point p)
+        {
+            foreach (var w in wall)
+            {
+                if (p == w)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 
-    class FoodFactory
+    enum Direction //определим перечисление направления движения змейки.
+    {
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN
+    }
+
+    class Snake
+    {
+        private List<Point> snake;
+
+        private Direction direction;
+        private int step = 1;
+        private Point tail;
+        private Point head;
+
+        bool rotate = true;
+
+        public Snake(int x, int y, int length) //Методы движения и поворота в зависимости он направления движения змейки.
+        {
+            direction = Direction.RIGHT;
+
+            snake = new List<Point>();
+            for (int i = x - length; i < x; i++)
+            {
+                Point p = (i, y, '*');
+                snake.Add(p);
+
+                p.Draw();
+            }
+        }
+        
+        public Point GetHead() => snake.Last();
+
+        public void Move()
+        {
+            head = GetNextPoint();
+            snake.Add(head);
+
+            tail = snake.First();
+            snake.Remove(tail);
+
+            tail.Clear();
+            head.Draw();
+
+            rotate = true;
+        }
+
+        public bool Eat(Point p)
+        {
+            head = GetNextPoint();
+            if (head == p)
+            {
+                snake.Add(head);
+                head.Draw();
+                return true;
+            }
+            return false;
+        }
+
+        public Point GetNextPoint()
+        {
+            Point p = GetHead();
+
+            switch (direction)
+            {
+                case Direction.LEFT:
+                    p.x -= step;
+                    break;
+                case Direction.RIGHT:
+                    p.x += step;
+                    break;
+                case Direction.UP:
+                    p.y -= step;
+                    break;
+                case Direction.DOWN:
+                    p.y += step;
+                    break;
+            }
+            return p;
+        }
+
+        public void Rotation(ConsoleKey key)
+        {
+            if (rotate)
+            {
+                switch (direction)
+                {
+                    case Direction.LEFT:
+                    case Direction.RIGHT:
+                        if (key == ConsoleKey.DownArrow)
+                            direction = Direction.DOWN;
+                        else if (key == ConsoleKey.UpArrow)
+                            direction = Direction.UP;
+                        break;
+                    case Direction.UP:
+                    case Direction.DOWN:
+                        if (key == ConsoleKey.LeftArrow)
+                            direction = Direction.LEFT;
+                        else if (key == ConsoleKey.RightArrow)
+                            direction = Direction.RIGHT;
+                        break;
+                }
+                rotate = false;
+            }
+
+        }
+
+        public bool IsHit(Point p)
+        {
+            for (int i = snake.Count - 2; i > 0; i--)
+            {
+                if (snake[i] == p)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    class FoodFactory //генерация еды, будет заниматься созданием еды внутри границ.
     {
         int x;
         int y;
@@ -104,21 +279,5 @@ namespace zmeika
             food = (random.Next(2, x - 2), random.Next(2, y - 2), ch);
             food.Draw();
         }
-
-        enum Direction
-        {
-            LEFT,
-            RIGHT,
-            UP,
-            DOWN
-        }
-
-        class Snake
-        {
-            private List<Point> snake;
-        }
-
     }
-
-
 }
